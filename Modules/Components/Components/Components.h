1d565fb6508b153cbcda19c744a916eda8ec00dd
Adauto Pinheiro <adautopinheiroo@gmail.com>
2020-06-28 18:45:02 -0300

    Create Components project

---
+++ Modules/Components/Components/Components.h
@@ -0,0 +1,19 @@
+//
+//  Components.h
+//  Components
+//
+//  Created by adauto.pinheiro on 28/06/20.
+//  Copyright © 2020 Adauto Pinheiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Components.
+FOUNDATION_EXPORT double ComponentsVersionNumber;
+
+//! Project version string for Components.
+FOUNDATION_EXPORT const unsigned char ComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Components/PublicHeader.h>
+
+
