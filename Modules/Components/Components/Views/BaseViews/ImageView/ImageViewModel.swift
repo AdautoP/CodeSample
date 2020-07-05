be8c2d5863c3c2b5efb9c4afa0b7f5333dd307ab
= <adautopinheiroo@gmail.com>
2020-07-05 09:53:38 -0300

    Improve ImageViewModel

--- Modules/Components/Components/Views/BaseViews/ImageView/ImageViewModel.swift
+++ Modules/Components/Components/Views/BaseViews/ImageView/ImageViewModel.swift
@@ -14,52 +14,34 @@ class ImageViewModel {
     
     private let disposeBag = DisposeBag()
     private let stateSubject = PublishSubject<Display<UIImage>>()
-    private let requestSubject = PublishSubject<Display<UIImage>>()
     
-    private(set) lazy var stateObservable: Observable<Display<UIImage>> = requestSubject.asObservable()
+    private(set) lazy var stateObservable: Observable<Display<UIImage>> = stateSubject.asObservable()
     
     private var disposable: Disposable?
     var emptyImage: UIImage
     
-    
-    
     init(emptyImage: UIImage = Asset.imageNotFound.image) {
         self.emptyImage = emptyImage
-        bind()
-    }
-    
-    func bind() {
-        requestSubject
-            .do(onNext: {
-                switch $0 {
-                case .success: self.disposable?.dispose()
-                default: break
-                }
-            })
-            .subscribe(onNext: { self.stateSubject.onNext($0 ) })
-            .disposed(by: disposeBag)
-        
     }
     
     func getImageFromUrl(_ urlString: String) {
         disposable?.dispose()
         if let cachedImage = Self.cache.object(forKey: urlString as NSString) {
-            requestSubject.onNext(.success(cachedImage))
+            return stateSubject.onNext(.success(cachedImage))
+        } else {
+            guard let url = URL(string: urlString) else {
+                return stateSubject.onNext(.success(emptyImage))
+            }
+            
+            let request = URLRequest(url: url)
+            disposable = URLSession
+                .shared
+                .rx
+                .data(request: request)
+                .map { (UIImage(data: $0) ?? self.emptyImage) }
+                .do(onNext: { Self.cache.setObject($0, forKey: urlString as NSString) })
+                .displayable(retryAction: nil)
+                .subscribe(onNext: { self.stateSubject.onNext($0) })
         }
-        
-        guard let url = URL(string: urlString) else {
-            requestSubject.onNext(.success(emptyImage))
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        disposable = URLSession
-            .shared
-            .rx
-            .data(request: request)
-            .map { (UIImage(data: $0) ?? self.emptyImage) }
-            .do(onNext: { Self.cache.setObject($0, forKey: urlString as NSString) })
-            .displayable(retryAction: nil)
-            .subscribe(onNext: { self.requestSubject.onNext($0) })
     }
 }
