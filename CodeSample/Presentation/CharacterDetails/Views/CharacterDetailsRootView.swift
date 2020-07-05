64dbf6a384efb630f4e48069a067c8035748ce85
= <adautopinheiroo@gmail.com>
2020-07-05 16:33:50 -0300

    Remove tableView

--- CodeSample/Presentation/CharacterDetails/Views/CharacterDetailsRootView.swift
+++ CodeSample/Presentation/CharacterDetails/Views/CharacterDetailsRootView.swift
@@ -13,22 +13,16 @@ class CharacterDetailsRootView: ScreenView {
     
     private let imageHeaderView = ImageHeaderView()
     
-    private lazy var tableView = BaseTableView() >> {
-        $0.delegate = self
-        $0.dataSource = self
-        $0.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.identifer)
-    }
-    
     private var imageHeight = NSLayoutConstraint()
     
     override func buildScreen() {
         render(.contentView(contentView))
     }
+    
     override func buildSubviews() {
         super.buildSubviews()
         
         contentView.addSubview(imageHeaderView)
-        contentView.addSubview(tableView)
     }
     
     override func buildConstraints() {
@@ -36,50 +30,9 @@ class CharacterDetailsRootView: ScreenView {
         
         imageHeaderView.edgesToSuperview(excluding: .bottom)
         imageHeight = imageHeaderView.heightToSuperview(multiplier: 0.3)
-        
-        tableView.topToBottom(of: imageHeaderView)
-        tableView.edgesToSuperview(excluding: .top)
     }
     
     func layout(_ character: Character) {
         imageHeaderView.layout(character.image)
-    }
-}
-
-extension CharacterDetailsRootView: BaseTableViewDelegate, BaseTableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell() >> { $0.textLabel?.text = "\(indexPath.row)" }
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard tableView.hasVisibleCells else { return }
-        let yOffset = scrollView.contentOffset.y
-        
-        let minOffset = -(view.frame.height / 3) - 100
-        let maxOffset = view.frame.height / 3 + 100
-        
-        if minOffset...0 ~= yOffset {
-            print(yOffset)
-            self.imageHeight.constant -= yOffset
-            self.layoutIfNeeded()
-        } else if 1...maxOffset ~= yOffset {
-            self.imageHeight.constant = -yOffset
-            self.layoutIfNeeded()
-        }
-    }
-    
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        
-        if imageHeight.constant > 0.0 {
-            self.imageHeight.constant = 0
-            UIView.animate(withDuration: 0.2, animations: {
-                self.layoutIfNeeded()
-            })
-        }
     }
 }
