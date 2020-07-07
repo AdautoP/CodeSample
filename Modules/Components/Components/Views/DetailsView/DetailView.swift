df9d6163b74e754a728864dd6759c5bb4b6baab5
= <adautopinheiroo@gmail.com>
2020-07-07 00:43:15 -0300

    Remove bouncing from detailView

--- Modules/Components/Components/Views/DetailsView/DetailView.swift
+++ Modules/Components/Components/Views/DetailsView/DetailView.swift
@@ -26,6 +26,7 @@ public class DetailView: AloeStackView {
         automaticallyHidesLastSeparator = true
         backgroundColor = AppColors.Grays.lightGray
         delegate = self
+        bounces = false
     }
     
     @available(*, unavailable)
